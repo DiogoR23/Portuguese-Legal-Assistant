8c1b7572f5c0366f8486c6d2a1b6775a13677a72
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-02-03 16:42:23 +0000

    Create the rag to communicate with the LLM

---
+++ ai_assistant/rag_pipeline.py
@@ -0,0 +1,27 @@
+from langchain.tools.retriever import create_retriever_tool
+import logging
+from cassandra.query import SimpleStatement
+
+def create_retriever_from_cassandra(vstore, name, description):
+    """Create a Retriever from cassandra Vector Store."""
+    try:
+        retriever = vstore.as_retriever()
+
+        retriever_tool = create_retriever_tool(
+            retriever=retriever,
+            name=name,
+            description=description
+        )
+
+        return retriever_tool
+
+    except Exception as e:
+        logging.error(f"Error creating retriever: {e}")
+
+
+def check_vector_store_data(session):
+    query = SimpleStatement("SELECT * FROM cassandra.vector_store;")
+    rows = session.execute(query)
+
+    for row in rows:
+        print(row)
