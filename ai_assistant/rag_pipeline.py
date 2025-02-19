3b0e6961be52eb1455ba97e5dfe44d65ee51b7d1
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-02-19 15:13:38 +0000

    Creating a RAG

--- ai_assistant/rag_pipeline.py
+++ ai_assistant/rag_pipeline.py
@@ -1,13 +1,33 @@
 from langchain.tools.retriever import create_retriever_tool
 from langchain_community.retrievers import BM25Retriever
+from langchain_core.documents import Document
 import logging
 from cassandra.query import SimpleStatement
 
 
-# def retrive_laws(vstore, ):
+def retriever_law(session, keyspace):
+    try:
+        query = f"SELECT id_articles, url, title, content FROM {keyspace}.articles"
+        data = session.execute(f"SELECT * FROM {keyspace}.articles")
+
+        docs = [
+            Document(
+                page_content=entry['content'],
+                metadata={'title': entry['title'], 'url': entry['url']}
+            )
+            for entry in data
+        ]
+
+        retriever_bm25 = BM25Retriever.from_documents(documents=docs)
+        retriever_bm25.k = 10
+
+        return retriever_bm25
+    
+    except Exception as e:
+        logging.error(f"Error creating BM25Retriever: {e}")
 
 
-def create_retriever_from_cassandra(vstore, name, description):
+def hybrid_retriever(vstore, name, description):
     """Create a Retriever from cassandra Vector Store."""
     try:
         retriever = vstore.as_retriever()
