61cc4a14708125a84553fe59b9bb9a36af66d4d8
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-02-20 16:59:25 +0000

    Run the scraper

---
+++ ai_development/scraper/run_scraper.py
@@ -0,0 +1,38 @@
+from scraper.create_cassandra_session import CreateCassandraSession 
+from scraper.scraper import crawl_site
+import logging
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+USERNAME = os.getenv('CASSANDRA_USERNAME')
+PASSWORD = os.getenv('CASSANDRA_PASSWORD')
+KEYSPACE = os.getenv('CASSANDRA_KEYSPACE')
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def main():
+    """Main function to execute the crawling and data saving"""
+    try:
+        session = CreateCassandraSession(username=USERNAME, password=PASSWORD)
+
+        session.clear_table()
+
+        start_url = 'https://diariodarepublica.pt/dr/detalhe/diario-republica/142-2024-873180956'
+        base_url = "https://diariodarepublica.pt"
+
+        if session:
+            crawl_site(start_url, base_url, session)
+
+            session.list_keyspaces()
+
+            session.list_tables(KEYSPACE)
+
+            session.view_table_data('articles', KEYSPACE)
+
+    except Exception as e:
+        logging.error(f"Error executing the principal function (main): {e}")
+
+
+if __name__ == '__main__':
+    main()
