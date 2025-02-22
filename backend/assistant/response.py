ccbe399ad080e1f054e53f4a53b6bf88680a7f22
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-02-22 18:38:29 +0000

    Function to integrate in the API

--- backend/assistant/response.py
+++ backend/assistant/response.py
@@ -1,21 +1,24 @@
-from cassandra.cluster import Cluster
-from dotenv import load_dotenv
-import os
-from langchain.tools.retriever import create_retriever_tool
-from .database import connect_to_cassandra
-from .cassandra_vectorstore import connect_vstore
-from .database import save_answer_question
-from langchain_community.vectorstores import Cassandra
-from langchain_openai import OpenAIEmbeddings
-import logging
-from langchain_community.utilities.cassandra import SetupMode
+from database import connect_to_cassandra, save_answer_question
+from cassandra_vectorstore import connect_vstore
+from rag_pipeline import rag_tool
+
 from langchain.agents import AgentExecutor, create_openai_tools_agent
+from cassandra.cluster import Cluster
+from langchain_community.vectorstores import Cassandra
+from langchain_community.utilities.cassandra import SetupMode
 from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
 from langchain.prompts import PromptTemplate
+from langchain.tools.retriever import create_retriever_tool
+
+from dotenv import load_dotenv
+import logging
+import os
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 BASE_URL = os.getenv("BASE_URL")
+CASSANDRA_KEYSPACE = os.getenv("CASSANDRA_KEYSPACE")
 
 def get_ai_response(user_input):
     """Process user input and return an AI response."""
@@ -23,43 +26,68 @@ def get_ai_response(user_input):
         session = connect_to_cassandra()
 
         vstore = connect_vstore(session=session)
-        retriever = vstore.as_retriever(search_kwargs={"k": 100})
-        tool = create_retriever_tool(
-            retriever=retriever,
-            name="law_search_tool",
-            description=("""Search for information about Portuguese laws.
-                            For any questions about some law or some doubts the user has about Portugues rules, you must use this tool!"""
-                        )
+
+        name = 'law_search'
+        description = (
+            """Search for information about Portuguese laws.
+            For any questions about some law or some doubts the user has about Portuguese rules, you must use this tool!
+            You must cite the article, with the article code and his title, every time your answer refers to a specific law or article."""
+        )
+
+        tool = rag_tool(
+            session=session,
+            name=name,
+            description=description,
+            keyspace=CASSANDRA_KEYSPACE,
+            vstore=vstore
         )
         tools = [tool]
 
-        # template = (
-        #     "You are an Intelligent assistant specialized in Portuguese law."
-        #     "Your role is to provide accurate and detailed information about Portuguese laws using the provided database"
-        #     "Ensure you"
-        # )
+        input_variables = ["input", "context", "agent_scratchpad"]
+
+        template = """
+            You are a legal assistant specialized in finding and explaining laws precisely. 
+            Whenever you provide an answer that refers to a specific law or article, **immediately cite the article code and title**, right after mentioning it, along with a brief explanation of what the article stipulates in relation to the situation described.
+            You should base your answer only on the information below. If the information is not there, **do not make it up**, just say that you did not find enough data.
+            Focus solely on providing the correct answer.
+            Speak only in Portuguese.
+
+            **Available Context**:
+            {context}  # This context includes detailed legal references, including article codes and titles.
+
+            **User Input**:
+            {input}
+
+            **Instructions for Responding**:
+            - **Contextualize** the answer based on the information provided.
+            - **Explain clearly and objectively**, especially when citing laws.
+            - **When referring to laws or articles**, immediately cite the **article code** and **title**, for example:
+                - "Artigo 213º do Código Civil - 'Divórcio e separação de bens'"
+            - **If the answer is inconclusive or more data or documents are required, state that more context is needed.**
+            - **If the cited article is not directly applicable, explain why it is not relevant.**
+
+            {agent_scratchpad}
+        """
 
         prompt_template = PromptTemplate(
-            input_variables=["input", "agent_scratchpad"],
-            template=("You are an intelligent assistant specialized in Portuguese law."
-                        "Your role is to provide accurate and detailed information about Portuguese laws using the provided database."
-                        "When answering user queries, refer to specific laws and articles where applicable."
-                        "Ensure your responses are precise and useful.\n\n"
-                        "Query: {input}\n"
-                        "{agent_scratchpad}")
+            input_variables=input_variables,
+            template=template
         )
 
         llm = ChatOpenAI(
             api_key=OPENAI_API_KEY,
             base_url=BASE_URL,
-            model="LM Studio Community/Meta-Llama-3-8B-Instruct-GGUF",
+            model="RichardErkhov/mistralai_-_Mistral-7B-Instruct-v0.3-gguf",
             temperature=0.8
         )
 
         agent = create_openai_tools_agent(llm=llm, tools=tools, prompt=prompt_template)
         agent_executor = AgentExecutor(agent=agent, tools=tools)
 
-        result = agent_executor.invoke({"input": user_input, "agent_scratchpad": ""})
+        retrieved_docs = tool.invoke(user_input)
+        context = "".join([doc for doc in retrieved_docs])
+
+        result = agent_executor.invoke({"input": user_input, "context": context, "agent_scratchpad": ""})
         response = result["output"]
 
         save_answer_question(session=session, input_history=[user_input], answers_history=[response])
