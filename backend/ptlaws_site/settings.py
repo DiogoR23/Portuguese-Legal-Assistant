983f3f4aca89487e9403e0ff538aa6aae654e10c
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-02-23 22:13:47 +0000

    Add the app and rest_framework to django ssettings

--- backend/ptlaws_site/settings.py
+++ backend/ptlaws_site/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'ptlaws_api.apps.PtlawsApiConfig'
 ]
 
 MIDDLEWARE = [
