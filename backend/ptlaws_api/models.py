9db97328efa57749d1d32782da7119e52a66cbfe
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-02-24 21:11:26 +0000

    Create Models Database

--- backend/ptlaws_api/models.py
+++ backend/ptlaws_api/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+import uuid
+from django_cassandra_engine.models import DjangoCassandraModel
+from cassandra.cqlengine import columns
 
 # Create your models here.
+class Article(DjangoCassandraModel):
+    id_articles = columns.UUID(primary_key=True, default=uuid.uuid4)
+    url = columns.Text(required=True)
+    title = columns.Text(required=True)
+    content = columns.Text(required=True)
+
+    class Meta:
+        db_table = 'articles'
+        keyspace = 'cassandra'
+
+
