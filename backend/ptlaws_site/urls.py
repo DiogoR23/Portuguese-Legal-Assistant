ebe6fca0c36744f8560f26e74fdaa7baa7611e46
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-02-24 19:39:40 +0000

    Create the main URLs

--- backend/ptlaws_site/urls.py
+++ backend/ptlaws_site/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api_auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('', include('ptlaws_api.urls'))
 ]
