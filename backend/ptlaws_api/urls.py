f5e3bbe45ed27f19c2a5edfb8eba8d2504cdc156
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-02-24 19:38:16 +0000

    Create the urls for access the API

--- backend/ptlaws_api/urls.py
+++ backend/ptlaws_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ArticleView, ConversationView
+from .views import ArticleView, CreateConversationView, GetConversationView
 
 urlpatterns = [
     path('articles/<uuid:id_article>/', ArticleView.as_view(), name="article-detail"),
-    path('conversations/', ConversationView.as_view(), name='conversation-create')
+    path('conversations/', CreateConversationView.as_view(), name='conversation-create'),
+    path('conversations/<uuid:user_id>', GetConversationView.as_view(), name='get-conversations')
 ]
