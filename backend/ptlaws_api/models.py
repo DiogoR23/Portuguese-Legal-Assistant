bec91cbc210b8096c3077a31138780cf629ea85a
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-02-25 23:25:29 +0000

    Connect Django Models with Cassandra Databse

--- backend/ptlaws_api/models.py
+++ backend/ptlaws_api/models.py
@@ -3,14 +3,44 @@ from django_cassandra_engine.models import DjangoCassandraModel
 from cassandra.cqlengine import columns
 
 # Create your models here.
-class Article(DjangoCassandraModel):
+class Articles(DjangoCassandraModel):
     id_articles = columns.UUID(primary_key=True, default=uuid.uuid4)
     url = columns.Text(required=True)
     title = columns.Text(required=True)
     content = columns.Text(required=True)
-
     class Meta:
         db_table = 'articles'
-        keyspace = 'cassandra'
 
 
+class AiAnswers(DjangoCassandraModel):
+    id_answers = columns.UUID(primary_key=True, default=uuid.uuid4)
+    content_answers = columns.Text(required=True)
+    class Meta:
+        db_table = 'ai_answers'
+
+
+class UserQuestions(DjangoCassandraModel):
+    id_questions = columns.UUID(primary_key=True, default=uuid.uuid4)
+    content_questions = columns.Text(required=True)
+    class Meta:
+        db_table = 'user_questions'
+
+
+class Users(DjangoCassandraModel):
+    id_user = columns.UUID(primary_key=True, default=uuid.uuid4)
+    name = columns.Text(required=True)
+    email = columns.Text(required=True)
+    password = columns.Text(required=True)
+    class Meta:
+        db_table = 'users'
+
+
+class Conversations(DjangoCassandraModel):
+    id_conversation = columns.UUID(primary_key=True, clustering_order='ASC', default=uuid.uuid4)
+    id_user = columns.UUID(partition_key=True, required=True)
+    date = columns.UUID(primary_key=True, clustering_order='DESC', required=True)
+    id_question = columns.UUID(required=True)
+    id_answer = columns.UUID(required=True)
+    class Meta:
+        db_table = 'conversations'
+        # get_pk_field = 
