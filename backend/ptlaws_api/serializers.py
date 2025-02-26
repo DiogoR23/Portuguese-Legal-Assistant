04ca7d987267c351a53f5faf25c9f0264c386b11
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-02-26 18:08:26 +0000

    Create Serializers

--- backend/ptlaws_api/serializers.py
+++ backend/ptlaws_api/serializers.py
@@ -1,25 +1,31 @@
 from rest_framework import serializers
-
-class ArticleSerializer(serializers.Serializer):
-    id_article = serializers.UUIDField()
-    title = serializers.CharField(max_length=200)
-    content = serializers.CharField()
-    url = serializers.URLField()
+from .models import Articles, AIAnswers, UserQuestions, Conversations, Users
 
 
-class AnswerSerializer(serializers.Serializer):
-    id_anwer = serializers.UUIDField()
-    content_answer = serializers.CharField()
+class ArticlesSerializer(serializers.Serializer):
+    class Meta:
+        model = Articles
+        fields = ['id_articles', 'url', 'title', 'content']
 
 
-class QuestionSerializer(serializers.Serializer):
-    id_question = serializers.UUIDField()
-    content_question = serializers.CharField()
+class AIAnswersSerializer(serializers.Serializer):
+    class Meta:
+        model = AIAnswers
+        fields = ['content_answers']
 
+
+class UserQuestionsSerializer(serializers.Serializer):
+    class Meta:
+        model = UserQuestions
+        fields = ['content_question']
+
+
+class UsersSerializer(serializers.Serializer):
+    class Meta:
+        model = Users
+        fields = ['name', 'email']
 
 class ConversationSerializer(serializers.Serializer):
-    id_conversation = serializers.UUIDField()
-    id_user = serializers.UUIDField()
-    id_question = serializers.UUIDField()
-    id_answer = serializers.UUIDField()
-    date = serializers.DateTimeField()
+    class Meta:
+        model = Conversations
+        fields = ['id_user', 'data', 'id_question', 'id_answer']
