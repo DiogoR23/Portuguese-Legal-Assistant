5a0d2992e05d1a48abccd4ce1721b421088e3614
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-02-26 21:46:26 +0000

    Change functions

--- backend/scraper/extractor.py
+++ backend/scraper/extractor.py
@@ -4,17 +4,17 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def extract_data_from_page(url, page):
+async def extract_data_from_page(url, page):
     """Extract title and content from a given page URL."""
     try:
-        page.goto(url)
-        page.wait_for_timeout(5000)
+        await page.goto(url)
+        await page.wait_for_timeout(5000)
 
-        page_html = page.content()
+        page_html = await page.content()
         soup = BeautifulSoup(page_html, 'html.parser')
 
         title = soup.find('h1').get_text(strip=True)
-        content = soup.find('div', class_ = 'content').get_text(strip=True)
+        content = soup.find('div', class_='content').get_text(strip=True)
 
         return {
             'url': url,
@@ -26,22 +26,23 @@ def extract_data_from_page(url, page):
         return None
 
 
-def extract_links(page):
+async def extract_links(page):
     """Extract links from the page."""
     try:
         containers = page.locator('div.list.list-group.OSFillParent')
         links = []
 
-        for index in range(containers.count()):
+        count = await containers.count()
+
+        for index in range(count):
             container = containers.nth(index)
-            container_html = container.inner_html()
+            container_html = await container.inner_html()
             soup = BeautifulSoup(container_html, 'html.parser')
 
             links.extend([a.get('href') for a in soup.find_all('a', href=True)])
-        
+
         return links
-    
+
     except Exception as e:
         logging.error(f"Error extracting links: {e}")
         return []
-
