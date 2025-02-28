953cf00267e2fff16d345a85f732e37ad9e94302
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-02-28 18:36:20 +0000

    Change the Settings

--- backend/ptlaws_site/settings.py
+++ backend/ptlaws_site/settings.py
@@ -64,10 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'ptlaws_site.urls'
 
