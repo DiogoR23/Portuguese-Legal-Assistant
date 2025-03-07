9a7c1b0895ab125507c35553776ee2b878ddfeb8
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-03-07 15:24:40 +0000

    Feat - Crete the application URLs

--- backend/ptlaws_api/urls.py
+++ backend/ptlaws_api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import RegisterView, LoginView, ChatView
 
 urlpatterns = [
-    path('users/', views.ListUserView.as_view(), name='user-list'),
-    path('create-user/', views.CreateUserView.as_view(), name='user-create'),
-    path('ptlawsai/', views.AIResponseView.as_view(), name='ptlaws-ai')
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("conversations/<uuid:conversation_id>/chat/", ChatView.as_view(), name="chat"),
 ]
