059b2f8a249fcf7b49e5dcc87de4891a8c8732ab
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-03-08 19:18:55 +0000

    Fix - Calling the right class models

--- backend/ptlaws_api/serializers.py
+++ backend/ptlaws_api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
-from .models import CustomUser, Message
+from .models import Users, Message
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,11 +15,11 @@ class RegisterSerializer(serializers.ModelSerializer):
     username = serializers.CharField()
     password = serializers.CharField()
     class Meta:
-        model = CustomUser
+        model = Users
         fields = ['id_user', 'email', 'username', 'password']
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        user = Users.objects.create_user(
             email=validated_data['email'],
             username=validated_data['username'],
             password=validated_data['password']
@@ -31,7 +31,7 @@ class LoginSerializer(serializers.Serializer):
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
-        user = CustomUser.objects.filter(email=data["email"]).first()
+        user = Users.objects.filter(email=data["email"]).first()
         if user and user.password == data["password"]:
             refresh = RefreshToken.for_user(user)
             return {"refresh": str(refresh), "access": str(refresh.access_token)}
