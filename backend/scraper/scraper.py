dda51a840d10dc2270a3fb38a501a453f8914bfd
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-03-08 17:49:03 +0000

    Perf - Optimise the crawler

--- backend/scraper/scraper.py
+++ backend/scraper/scraper.py
@@ -1,15 +1,27 @@
-from playwright.async_api import async_playwright
-import logging
 from extractor import extract_data_from_page, extract_links
+
+from playwright.async_api import async_playwright
+
+import logging
+import random
+import asyncio
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+HEADLESS_MODE = True # Don't see the Browser while running main
+MAX_RETRIES = 3 # Number of attempts in case of error
 
 async def crawl_site(start_url, base_url, session):
     """Crawl the site starting from the start_url and save the data to Cassandra."""
+
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True)
+        browser = await p.chromium.launch(headless=HEADLESS_MODE)
         page = await browser.new_page()
+
+        # Simulate a real user
+        await page.set_extra_http_headers({
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36"
+        })
 
         visited = set()
         to_visit = [start_url]
@@ -20,15 +32,24 @@ async def crawl_site(start_url, base_url, session):
                 continue
 
             visited.add(current_url)
-            data = await extract_data_from_page(current_url, page)
-            if data:
-                session.save_data([data])
 
-            await page.goto(current_url)
-            await page.wait_for_timeout(5000)
-            new_links = await extract_links(page) 
+            # Retry logic to avoid failures on some pages
+            for attempt in range(MAX_RETRIES):
+                try:
+                    logging.info(f"Visiting: {current_url} (Attempt {attempt + 1}/{MAX_RETRIES})")
+                    data = await extract_data_from_page(current_url, page)
+                    if data:
+                        session.save_data([data])
+                    break # If the exctraction is a success, break the attempt loop 
 
-            full_links = [link if link.startswith(base_url) else base_url + link for link in new_links]
+                except Exception as e:
+                    logging.warning(f"Error extracting {current_url}: {e}")
+                    await asyncio.sleep(random.uniform(5, 10)) # Short break before the new attempt.
+
+            await asyncio.sleep(random.uniform(2, 5)) # Random delay to avoid blocking
+
+            new_links = await extract_links(page)
+            full_links = {link if link.startswith(base_url) else base_url + link for link in new_links}
 
             for link in full_links:
                 if link not in visited:
