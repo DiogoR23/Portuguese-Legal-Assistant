203bcb0ff10f908e6ac6fc1c92daabaf90f39b18
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-03-08 19:18:40 +0000

    Fix - Email and username must be unique, creating if statement

--- backend/ptlaws_api/models.py
+++ backend/ptlaws_api/models.py
@@ -7,8 +7,7 @@ import datetime
 class Conversations(DjangoCassandraModel):
     id_conversation = columns.UUID(primary_key=True, default=uuid.uuid4)
     id_user = columns.UUID(required=True)
-    id_questions = columns.UUID(required=True)
-    id_answer = columns.UUID(required=True)
+    message_ids = columns.List(columns.UUID)
     title = columns.Text()
     created_at = columns.DateTime(default=datetime.datetime.utcnow)
 
@@ -25,12 +24,17 @@ class Message(DjangoCassandraModel):
     class Meta:
         db_table = "messages"
 
-
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, username, password=None):
         if not email:
-            raise ValueError('O email é obrigatório')
-        
+            raise ValueError('Email is required!')
+
+        if Users.objects.filter(email=email).first():
+            raise ValueError("This email is already in use!")
+
+        if Users.objects.filter(username=username).first():
+            raise ValueError("This username is already in use!")
+
         user = self.model(
             id_user=uuid.uuid4(),
             email=email.lower(),
@@ -49,15 +53,26 @@ class CustomUserManager(BaseUserManager):
         
         return user
 
-class Users(DjangoCassandraModel):
+class Users(DjangoCassandraModel, AbstractBaseUser):
     id_user = columns.UUID(primary_key=True, default=uuid.uuid4)
     email = columns.Text(required=True, index=True)
-    name = columns.Text(required=True)
+    username = columns.Text(required=True, index=True)
     password = columns.Text(required=True)
 
+    is_active = columns.Boolean(default=True)
+    is_staff = columns.Boolean(default=False)
+    is_superuser = columns.Boolean(default=False)
+
+    objects = CustomUserManager()
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'password']
+    REQUIRED_FIELDS = ['username']
+
+    def save(self, *args, **kwargs):
+        if self.password and not self.password.startswith('pbkdf2_sha256$'):
+            self.set_password(self.password)
+        
+        super().save(*args, **kwargs)
 
     class Meta:
         db_table = "users"
-        managed = False
