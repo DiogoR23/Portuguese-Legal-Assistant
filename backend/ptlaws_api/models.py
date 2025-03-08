a5d7b2f7a743c061bbe7227d00adf9edb9f09a3d
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-03-08 17:50:14 +0000

    Style - Optimise Models in Django

--- backend/ptlaws_api/models.py
+++ backend/ptlaws_api/models.py
@@ -4,9 +4,11 @@ from cassandra.cqlengine import columns
 import uuid
 import datetime
 
-class Conversation(DjangoCassandraModel):
+class Conversations(DjangoCassandraModel):
     id_conversation = columns.UUID(primary_key=True, default=uuid.uuid4)
     id_user = columns.UUID(required=True)
+    id_questions = columns.UUID(required=True)
+    id_answer = columns.UUID(required=True)
     title = columns.Text()
     created_at = columns.DateTime(default=datetime.datetime.utcnow)
 
@@ -16,8 +18,8 @@ class Conversation(DjangoCassandraModel):
 class Message(DjangoCassandraModel):
     id_message = columns.UUID(primary_key=True, default=uuid.uuid4)
     id_conversation = columns.UUID(required=True)
-    sender = columns.Text()
-    content = columns.Text()
+    sender = columns.Text(required=True)
+    content = columns.Text(required=True)
     created_at = columns.DateTime(default=datetime.datetime.utcnow)
 
     class Meta:
@@ -28,32 +30,34 @@ class CustomUserManager(BaseUserManager):
     def create_user(self, email, username, password=None):
         if not email:
             raise ValueError('O email é obrigatório')
+        
         user = self.model(
             id_user=uuid.uuid4(),
             email=email.lower(),
             username=username
         )
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
+        
         return user
 
     def create_superuser(self, email, username, password):
         user = self.create_user(email, username, password)
         user.is_superuser = True
         user.is_staff = True
-        user.save(using=self._db)
+        user.save()
+        
         return user
 
-class CustomUser(DjangoCassandraModel, AbstractBaseUser):
+class Users(DjangoCassandraModel):
     id_user = columns.UUID(primary_key=True, default=uuid.uuid4)
     email = columns.Text(required=True, index=True)
-    username = columns.Text(required=True)
+    name = columns.Text(required=True)
     password = columns.Text(required=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    objects = CustomUserManager()
+    REQUIRED_FIELDS = ['name', 'password']
 
     class Meta:
         db_table = "users"
+        managed = False
