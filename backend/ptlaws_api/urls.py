ff417686b6ff92cec1647474f8cfbbea368653ca
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-03-16 17:01:45 +0000

    Create urls for the api

--- backend/ptlaws_api/urls.py
+++ backend/ptlaws_api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import RegisterView, LoginView
+# from .views import RegisterView, LoginView, ProtectedView,
+from .views import ChatView
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+#     path("register/", RegisterView.as_view(), name="register"),
+#     path("login/", LoginView.as_view(), name="login"),
+#     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+#     path("protected/", ProtectedView.as_view(), name="protected"),
+    path("chat/", ChatView.as_view(), name="chat"),
 ]
