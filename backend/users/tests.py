44c1406f3090185f322350fdaa8933238d106bc0
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-03-22 21:24:35 +0000

    Create the tests, to check if everything is ok

---
+++ backend/users/tests.py
@@ -0,0 +1,53 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+class UserAuthTests(TestCase):
+    def setUp(self):
+        self.client_api = APIClient()
+        self.register_url = "/api/users/register/"
+        self.login_url = "/api/users/login/"
+        self.token_refresh_url = "/api/users/token/refresh/"
+        self.protected_url = "/api/users/protected/"
+
+        self.user_data = {
+            "email": "test@example.com",
+            "username": "TestUser",
+            "password": "testpass123",
+            "password2": "testpass123"
+        }
+
+    def test_register_user(self):
+        response = self.client_api.post(self.register_url, self.user_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn("user", response.data)
+
+    def test_login_user(self):
+        self.client_api.post(self.register_url, self.user_data, format='json')
+        response = self.client_api.post(self.login_url, {
+            "email": self.user_data['email'],
+            "password": self.user_data['password']
+        }, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("access_token", response.data)
+        self.assertIn("refresh_token", response.data)
+
+        self.access_token = response.data['access_token']
+        self.refresh_token = response.data['refresh_token']
+
+    def test_access_protected_route(self):
+        self.test_login_user()
+        self.client_api.credentials(HTTP_AUTHORIZATION=f"Bearer {self.access_token}")
+        response = self.client_api.get(self.protected_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_refresh_token(self):
+        self.test_login_user()
+        response = self.client_api.post(self.token_refresh_url, {
+            "refresh": self.refresh_token
+        }, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("access", response.data)
