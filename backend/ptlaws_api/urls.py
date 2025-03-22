5535657289893c2a22479b3ddd5ecebb8559efca
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-03-22 21:19:16 +0000

    Change the urls

--- backend/ptlaws_api/urls.py
+++ backend/ptlaws_api/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import RegisterView, LoginView, ProtectedView, ChatView
+from .views import ChatView
 
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("protected/", ProtectedView.as_view(), name="protected"),
     path("chat/", ChatView.as_view(), name="chat"),
 ]
