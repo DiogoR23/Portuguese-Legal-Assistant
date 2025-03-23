a15704e112d27735ca4d59616e1ecbe62b996135
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-03-23 18:24:46 +0000

    Creating Serializers for Messages and Conversations

--- backend/ptlaws_api/serializers.py
+++ backend/ptlaws_api/serializers.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
-import bcrypt
 
-from .models import Message
+from .models import Message, Conversations
 
-class MessageSerializer(serializers.ModelSerializer):
+class MessageSerializer(serializers.Serializer):
+    id_message = serializers.UUIDField()
+    id_conversation = serializers.UUIDField()
+    sender = serializers.CharField()
+    content = serializers.CharField()
+    created_at = serializers.DateTimeField()
     class Meta:
         model = Message
         fields = ['id_message', 'id_conversation', 'sender', 'content', 'created_at']
+
+
+class ConversationSerializer(serializers.Serializer):
+    id_conversation = serializers.UUIDField()
+    user_id = serializers.UUIDField()
+    message_ids = serializers.ListField()
+    title = serializers.CharField()
+    created_at = serializers.DateTimeField()
+    class Meta:
+        model = Conversations
+        fields = ['id_conversation', 'user_id', 'message_ids', 'title', 'created_at']
