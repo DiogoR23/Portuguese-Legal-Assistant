615014c5468387772c53edd81e1081ae2102183e
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-03-29 19:34:07 +0000

    Create new URLs for get the acces for conversations and messages

--- backend/ptlaws_api/urls.py
+++ backend/ptlaws_api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import ChatView
+from .views import ChatView, ListConversationsMessagesView, ListUserConversationsView, CreateConversationView
 
 
 urlpatterns = [
     path("chat/", ChatView.as_view(), name="chat"),
+    path("conversations/", ListUserConversationsView.as_view(), name="list-conversations"),
+    path("conversations/<uuid:conversation_id>/messages/", ListConversationsMessagesView.as_view(), name="conversation-messages"),
+    path("create/conversations/", CreateConversationView.as_view(), name="create-conversation")
 ]
