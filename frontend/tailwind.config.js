256835ebbeccd31c74a94be5a71d82bfdccf83d2
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-03-29 19:35:18 +0000

    Installing new packages, edit tailwind, vite configurations

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
+    darkMode: 'class',
     content: [
+		'./index.html',
 		'./src/**/*.{js,jsx,ts,tsx}',
 	],
   theme: {
