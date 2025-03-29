f85778bc5e556dee8d47765ca4714222a4f2143e
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-03-29 11:56:50 +0000

    Create the routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // <- importante
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App'
+import AuthPage from './pages/AuthPage';
+import ChatPage from './pages/ChatPage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AuthPage />} />
+        <Route path='/chat' element={<ChatPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
 
