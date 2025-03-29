0534eea7109070df94d4e25ec4ce54d42b87e6b2
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-03-29 19:36:14 +0000

    Create the routes for each page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App'
-import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 import ChatPage from './pages/ChatPage';
+import RegisterPage from './pages/RegisterPage';
+import LayoutWithTheme from './layouts/LayoutWithTheme';
+import './index.css'
+import './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<AuthPage />} />
-        <Route path='/chat' element={<ChatPage />} />
-      </Routes>
+    <Routes>
+      <Route element={<LayoutWithTheme />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chat" element={<ChatPage />} />
+    </Route>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
+
     </BrowserRouter>
   </React.StrictMode>
 )
