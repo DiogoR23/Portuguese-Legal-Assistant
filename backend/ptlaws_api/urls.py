a5a37ebcac844e5d07630fdc98362ee3ef073b71
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-04-11 17:57:32 +0100

    create the api url for deleting conversation

--- backend/ptlaws_api/urls.py
+++ backend/ptlaws_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import ChatView, ListConversationsMessagesView, ListUserConversationsView, CreateConversationView
+from .views import ChatView, ListConversationsMessagesView, ListUserConversationsView, CreateConversationView, DeleteConversationView
 
 
 urlpatterns = [
     path("chat/", ChatView.as_view(), name="chat"),
     path("conversations/", ListUserConversationsView.as_view(), name="list-conversations"),
     path("conversations/<uuid:conversation_id>/messages/", ListConversationsMessagesView.as_view(), name="conversation-messages"),
-    path("create/conversations/", CreateConversationView.as_view(), name="create-conversation")
+    path("create/conversations/", CreateConversationView.as_view(), name="create-conversation"),
+    path("conversations/<uuid:conversation_id>/", DeleteConversationView.as_view(), name="delete-conversation"),
 ]
