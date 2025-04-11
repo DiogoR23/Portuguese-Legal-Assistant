7f447e03319ae2dc623ab5478f9e952890cc835b
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-04-11 17:58:17 +0100

    Call the backend APIs for the ChatPage

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,3 +6,11 @@ export const registerUser = (data) => API.post("users/register/", data);
 export const sendMessage = (data) => API.post("ai/chat/", data);
 export const getUserInfo = () => API.get('protected/');
 
+// Conversations
+export const fetchUserConversations = () => API.get("ai/conversations/");
+export const fetchConversationMessages = (converdsationId) => 
+    API.get(`ai/conversations/${converdsationId}/messages`);
+export const createConversation = (title = "") =>
+    API.post("ai/create/conversations/", { title });
+export const deleteConversation = (conversationId) =>
+    API.delete(`ai/conversations/${conversationId}/`);
