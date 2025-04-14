f93d298f6fe2d5ce6fa25eeb01b31a5b065beb9e
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-04-14 23:58:55 +0100

    Rename File

---
+++ frontend/postcss.config.cjs
@@ -0,0 +1,9 @@
+const { default: autoprefixer } = require("autoprefixer");
+const { default: plugin } = require("tailwindcss");
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
