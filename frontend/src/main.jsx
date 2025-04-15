a07ce4ab63826837c399ccf6cbb9dd007e798f4e
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-04-15 23:03:40 +0100

    Main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,28 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App'
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import ChatPage from './pages/ChatPage';
-import RegisterPage from './pages/RegisterPage';
-import LayoutWithTheme from './layouts/LayoutWithTheme';
-import './index.css'
-import './theme';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route element={<LayoutWithTheme />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/chat" element={<ChatPage />} />
-    </Route>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-    </Routes>
-
+      <App />
     </BrowserRouter>
   </React.StrictMode>
-)
-
+);
