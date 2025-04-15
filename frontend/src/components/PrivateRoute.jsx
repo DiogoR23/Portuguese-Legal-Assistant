8201d7edd4688e8a615688df14c88c050d81b547
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-04-15 23:03:48 +0100

    Editing files for the private router

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,9 +1,30 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import API from '@/services/axiosInstance';
 
-const PrivateRoute = ({ children })  => {
-    const token = localStorage.getItem("access");
-    return token ? children : <Navigate to="/login" replace />;
+const PrivateRoute = () => {
+  const [authChecked, setAuthChecked] = useState(false);
+  const [isValid, setIsValid] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await API.get('users/protected/');
+        setIsValid(true);
+      } catch (err) {
+        console.error("Não autenticado:", err);
+        setIsValid(false);
+      } finally {
+        setAuthChecked(true);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (!authChecked) return null;
+
+  return isValid ? <Outlet /> : <Navigate to={"/login"} replace />
 };
 
 export default PrivateRoute;
