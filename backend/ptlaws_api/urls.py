bdd909594922244b2d3b3b5b1ad459ab97b12c73
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-04-24 15:42:39 +0100

    Update the urls, add the UpdateConversationTitleView, so it can be called from the frontend

--- backend/ptlaws_api/urls.py
+++ backend/ptlaws_api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ChatView, ListConversationsMessagesView, ListUserConversationsView, CreateConversationView, DeleteConversationView
+from .views import ChatView, ListConversationsMessagesView, ListUserConversationsView, CreateConversationView, DeleteConversationView, UpdateConversationTitleView
 
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path("conversations/<uuid:conversation_id>/messages/", ListConversationsMessagesView.as_view(), name="conversation-messages"),
     path("create/conversations/", CreateConversationView.as_view(), name="create-conversation"),
     path("conversations/<uuid:conversation_id>/", DeleteConversationView.as_view(), name="delete-conversation"),
+    path("conversations/<uuid:conversation_id>/title/", UpdateConversationTitleView.as_view(), name="update-conversation-title"),
 ]
