85bc4b0c245ccbbdfc5f84b2ce4574d733ae609e
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2025-04-24 15:41:03 +0100

    Update the sendMessage API so the user can stop AI.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,15 +2,38 @@ import API from "@/services/axiosInstance";
 
 // Authentication
 export const loginUser = (data) => API.post("users/login/", data);
+
 export const registerUser = (data) => API.post("users/register/", data);
+
 export const getUserInfo = () => API.get('protected/');
 
+
 // AI Connection
-export const sendMessage = (payload) => API.post("ai/chat/", payload);
+export const sendMessage = (payload, controller) =>
+    fetch("http://localhost:8000/api/ai/chat/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("access")}`,
+      },
+      body: JSON.stringify(payload),
+      signal: controller.signal,
+    }).then(async (res) => {
+      if (!res.ok) throw new Error("Erro na resposta do servidor");
+      const data = await res.json();
+      return data;
+    });
+
 export const fetchUserConversations = () => API.get("ai/conversations/");
-export const fetchConversationMessages = (converdsationId) => 
+
+export const fetchConversationMessages = (converdsationId) =>
     API.get(`ai/conversations/${converdsationId}/messages`);
+
 export const createConversation = (title = "") =>
     API.post("ai/create/conversations/", { title });
+
 export const deleteConversation = (conversationId) =>
     API.delete(`ai/conversations/${conversationId}/`);
+
+export const updateConversation = (conversationId, title) =>
+    API.put(`ai/conversations/${conversationId}/title/`, { title });
